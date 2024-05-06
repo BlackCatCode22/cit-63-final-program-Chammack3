a62c09c2d2ea5ed88de8c37c28d9a57ec21e09bc
Chammack3 <157452345+Chammack3@users.noreply.github.com>
2024-05-05 23:25:49 -0700

    FinalSeachComplexity.java

---
+++ FinalSeachComplexity.java
@@ -0,0 +1,80 @@
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class SearchComplexity {
+
+    // Linear Search Method this is where I added array length and also iterations.    
+    public static int linearSearch(int[] array, int target) {
+        int iterations = 0;
+        for (int i = 0; i < array.length; i++) {
+            iterations++;
+            if (array[i] == target) {
+                System.out.println("Linear search iterations: " + iterations);
+                return i;  // Returns the index of the found element
+            }
+        }
+        System.out.println("Linear search iterations: " + iterations);
+        return -1;  // Target that is not found
+    }
+
+    // Binary Search Method (Iterative)
+    public static int binarySearch(int[] array, int target) {
+        return binarySearch(array, target, 0, array.length - 1, 0);
+    }
+
+    // Recursive Binary Search Method
+    public static int binarySearch(int[] array, int target, int left, int right, int iterations) {
+        if (left <= right) {
+            int mid = left + (right - left) / 2;
+            iterations++;
+            if (array[mid] == target) {
+                System.out.println("Binary search iterations: " + iterations);
+                return mid;
+            }
+            if (array[mid] < target) {
+                return binarySearch(array, target, mid + 1, right, iterations);
+            } else {
+                return binarySearch(array, target, left, mid - 1, iterations);
+            }
+        }
+        System.out.println("Binary search iterations: " + iterations);
+        return -1;  // Target not found
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Enter number of elements in array:");
+            int n = scanner.nextInt();
+            if (n <= 0) {
+                System.out.println("Array size should be greater than 0.");
+                return;
+            }
+            int[] array = new int[n];
+
+            System.out.println("Enter elements:");
+            for (int i = 0; i < n; i++) {
+                array[i] = scanner.nextInt();
+            }
+
+            System.out.println("Enter target number to search:");
+            int target = scanner.nextInt();
+
+            // Linear Search
+            int linearResult = linearSearch(array, target);
+            System.out.println((linearResult == -1) ? "Target not found by linear search." :
+                    "Target found by linear search at index: " + linearResult);
+
+            // Binary Search
+            Arrays.sort(array);
+            int binaryResult = binarySearch(array, target);
+            System.out.println((binaryResult == -1) ? "Target not found by binary search." :
+                    "Target found by binary search at index: " + binaryResult);
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter integers only.");
+        } finally {
+            scanner.close();
+        }
+    }
+}
